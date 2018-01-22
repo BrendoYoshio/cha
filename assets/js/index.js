604d4ec84d88535ff301504e2a5430c40d6a88ec
Brendo Yoshio Furyama de Oliveira <brendo.yoshio@gmail.com>
2018-01-22 00:37:48 -0200

    Responsive

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,6 +13,8 @@ jQuery(document).ready(function() {
       ctx.fillText(cnt, x, y);
     }
   }
+  var xResponsive = 0;
+  var lastLetter = "";
   function desenhar() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(img,0,0);
@@ -22,17 +24,24 @@ jQuery(document).ready(function() {
     ctx.fillStyle = '#ff9e94';
     ctx.font = `173px "${FONT_NAME_1}"`;
     ctx.textAlign='start';
-    // ctx.fillText(jQuery("#name2").val(), canvas.width/2-26, canvas.height-530);
-    validador("#name2", canvas.width/2-26, canvas.height-530, "Nome 2");
+    var txt = jQuery("#name2").val();
+    if (ctx.measureText(txt).width > 500) {
+      lastLetter += jQuery("#name2").val().slice(jQuery("#name2").val().length-1);
+      console.log(lastLetter);
+      xResponsive = ctx.measureText(lastLetter).width;
+    } else {
+      xResponsive = 0;
+      lastLetter = "";
+    }
+    validador("#name2", canvas.width/2-26-xResponsive, canvas.height-530, "Nome 2");
     ctx.textAlign='center';
-    // ctx.fillText(jQuery("#name1").val(), canvas.width/2, canvas.height-677);
     validador("#name1", canvas.width/2, canvas.height-677, "Nome 1");
     ctx.font = `89px "${FONT_NAME_1}"`;
     validador("#data", canvas.width/2, canvas.height-415, "00.00.00");
     ctx.fillStyle = '#b39789';
     ctx.font = `278px "${FONT_NAME_2}"`;
     ctx.rotate(-20 * Math.PI / 180);
-    ctx.translate(-500, 60);
+    ctx.translate(-500-xResponsive, 60-xResponsive/2);
     ctx.fillText('&', canvas.width/2-94, canvas.height-535);
     ctx.resetTransform();
   }
@@ -48,6 +57,7 @@ jQuery(document).ready(function() {
 
   jQuery("input").on('keyup', function(e) {
     desenhar();
+    // console.log(jQuery("#name2").val().length);
   });
 
   jQuery(img).attr("src", 'assets/img/bg.png')
